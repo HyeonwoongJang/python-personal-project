d9d75d9ae7820acd0f22ded2d81cdfe26c7dbe90
HyeonwoongJang <hyeonwoongjang01@gmail.com>
2023-08-24 20:01:30 +0900

    my_up_down_game_수정본

--- my_up_down_game_for.py
+++ my_up_down_game_for.py
@@ -2,40 +2,89 @@ import random
 
 def up_down_game():
 
-    records = []
-    
+    min_record = 0
+
     for search_num in range(1,10000):
             print('---------------------------------------')
             print('Up & Down 게임을 시작합니다.')
             print('1~100 사이의 숫자를 입력하세요.')
             rn = random.randint(1, 100)
+            # print(rn)
             tries = 0
             for search_num in range(1,10000):
+                tries += 1
+                print('---------------------------------------')
+                print(f"# {tries}번째 시도")
                 try :
                     print('---------------------------------------')
                     input_num = int(input('숫자를 입력하세요 : '))
-                    tries += 1
-                    records.append(tries)
-                    max_record = max(records)
-                    if (0 > input_num or 100 < input_num):
+                    if (1 > input_num or 100 < input_num):
                         print('1~100 사이의 숫자를 입력하세요')
-                    elif (search_num <= input_num < rn):
-                        print('UP')
-                    elif (input_num > rn >= search_num or search_num >= input_num > rn):
-                        print('DOWN')
-                    elif (input_num == rn <= search_num or input_num == rn >= search_num):
+                        continue
+                    if (input_num == rn):
+                        print('---------------------------------------')
                         print(tries, '번째 시도에 맞혔습니다.')
-                        records.append(tries)
+                        if tries <= min_record:
+                            min_record = tries
+                            print('---------------------------------------')
+                            print(f"최저 시도 기록 : {min_record}번")
+                        else:
+                            print('---------------------------------------')
+                            print(f"최저 시도 기록 : {tries}번")
                         break 
+                    elif (input_num < rn):
+                        print('UP')
+                    else:
+                        print('DOWN')
                 except ValueError:
                     print('정수로 입력해주세요.')
-            restart = str(input('한 판 더? (Y/N) '))
+
+            restart = input('한 판 더? (Y/N) ')
             if restart.upper() == 'Y' :
                 continue
             else :
                 print('---------------------------------------')
-                print(f'최고 시도 횟수 : {max_record}번')
+                print(f'최저 시도 기록 : {tries}번')
                 print('Up & Down 게임을 종료합니다.')
                 break
+# import random
+
+# def up_down_game():
+
+#     records = []
+    
+#     for search_num in range(1,10000):
+#             print('---------------------------------------')
+#             print('Up & Down 게임을 시작합니다.')
+#             print('1~100 사이의 숫자를 입력하세요.')
+#             rn = random.randint(1, 100)
+#             tries = 0
+#             for search_num in range(1,10000):
+#                 try :
+#                     print('---------------------------------------')
+#                     input_num = int(input('숫자를 입력하세요 : '))
+#                     tries += 1
+#                     records.append(tries)
+#                     max_record = max(records)
+#                     if (0 > input_num or 100 < input_num):
+#                         print('1~100 사이의 숫자를 입력하세요')
+#                     elif (search_num <= input_num < rn):
+#                         print('UP')
+#                     elif (input_num > rn >= search_num or search_num >= input_num > rn):
+#                         print('DOWN')
+#                     elif (input_num == rn <= search_num or input_num == rn >= search_num):
+#                         print(tries, '번째 시도에 맞혔습니다.')
+#                         records.append(tries)
+#                         break 
+#                 except ValueError:
+#                     print('정수로 입력해주세요.')
+#             restart = str(input('한 판 더? (Y/N) '))
+#             if restart.upper() == 'Y' :
+#                 continue
+#             else :
+#                 print('---------------------------------------')
+#                 print(f'최고 시도 횟수 : {max_record}번')
+#                 print('Up & Down 게임을 종료합니다.')
+#                 break
 
 up_down_game()
--- my_up_down_game_while.py
+++ my_up_down_game_while.py
@@ -2,41 +2,91 @@ import random
 
 def up_down_game():
 
-    records = []
+    min_record = 0
 
     while True :
             print('---------------------------------------')
             print('Up & Down 게임을 시작합니다.')
             print('1~100 사이의 숫자를 입력하세요.')
             rn = random.randint(1, 100)
+            print(rn)
             tries = 0
             while True :
+                tries += 1
+                print('---------------------------------------')
+                print(f"# {tries}번째 시도")
                 try :
                     print('---------------------------------------')
                     input_num = int(input('숫자를 입력하세요 : '))
-                    tries += 1
-                    records.append(tries)
-                    max_record = max(records)
-                    if (0 > input_num or 100 < input_num):
+                    if (1 > input_num or 100 < input_num):
                         print('1~100 사이의 숫자를 입력하세요')
+                        continue
+                    if (input_num == rn):
+                        print('---------------------------------------')
+                        print(tries, '번째 시도에 맞혔습니다.')
+                        if tries <= min_record:
+                            min_record = tries
+                            print('---------------------------------------')
+                            print(f"최저 시도 기록 : {min_record}번")
+                        else:
+                            print('---------------------------------------')
+                            print(f"최저 시도 기록 : {tries}번")
+                        break 
                     elif (input_num < rn):
                         print('UP')
-                    elif (input_num > rn):
-                        print('DOWN')
                     else:
-                        print('---------------------------------------')
-                        print(tries, '번째 시도에 맞혔습니다. ')
-                        break
+                        print('DOWN')
                 except ValueError:
                     print('정수로 입력해주세요.')
 
-            restart = str(input('한 판 더? (Y/N) '))
+            restart = input('한 판 더? (Y/N) ')
             if restart.upper() == 'Y' :
                 continue
             else :
                 print('---------------------------------------')
-                print(f'최대 시도 횟수 : {max_record}번')
+                print(f'최저 시도 기록 : {tries}번')
                 print('Up & Down 게임을 종료합니다.')
                 break
+
+# import random
+
+# def up_down_game():
+
+#     records = []
+
+#     while True :
+#             print('---------------------------------------')
+#             print('Up & Down 게임을 시작합니다.')
+#             print('1~100 사이의 숫자를 입력하세요.')
+#             rn = random.randint(1, 100)
+#             tries = 0
+#             while True :
+#                 try :
+#                     print('---------------------------------------')
+#                     input_num = int(input('숫자를 입력하세요 : '))
+#                     tries += 1
+#                     records.append(tries)
+#                     max_record = max(records)
+#                     if (0 > input_num or 100 < input_num):
+#                         print('1~100 사이의 숫자를 입력하세요')
+#                     elif (input_num < rn):
+#                         print('UP')
+#                     elif (input_num > rn):
+#                         print('DOWN')
+#                     else:
+#                         print('---------------------------------------')
+#                         print(tries, '번째 시도에 맞혔습니다. ')
+#                         break
+#                 except ValueError:
+#                     print('정수로 입력해주세요.')
+
+#             restart = str(input('한 판 더? (Y/N) '))
+#             if restart.upper() == 'Y' :
+#                 continue
+#             else :
+#                 print('---------------------------------------')
+#                 print(f'최대 시도 횟수 : {max_record}번')
+#                 print('Up & Down 게임을 종료합니다.')
+#                 break
 
 up_down_game()
